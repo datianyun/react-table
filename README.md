8b893e7b8f46f609a0bac04d254aea25b2f4039e
云 田 <yuntian@yundeMacBook-Pro.local>
2016-12-27 17:07:02 +0800

    示例代码

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-#react
+# react-table
+
+table组件
+
+[在线demo](http://datianyun.github.io/table "Title")
+### Installation
+``` sh
+npm install reactui-table
+```
+
+### API
+
+### props
+
+|name|type|default| description|
+|-----|---|--------|----|
+|columns | array |- | 表格头部|
+|tdata | array | -| 表格数据 |
+### Demo
+
+``` sh
+npm run start
+```
+
+http://localhost:8888/example/
+
+### Usage
+``` javascript
+let columns = ['示例1','示例2','示例3','操作']
+let tdata = [{
+    id : 1,
+    name : 'hello',
+    email : 'qq@qq.com'
+},{
+    id : 2,
+    name : 'admin',
+    email : 'admin@qq.com'
+}]
+let action = {
+    className : 'col-action clearfix',
+    type : 'html',
+    click : this.handleclick.bind(this),
+    value : ''
+}
+let label = '<a class="label '+'add'+'"' + ' data-status="' + '1'+'">' +'添加'+'</a>'
+action.value = label
+let cdata = [[1,"hello","qq@qq.com",action],[2,"admin","admin@qq.com",action]]
+
+<Table columns={columns} tdata={cdata}></Table>
+```
--- example/app.js
+++ example/app.js
@@ -8,15 +8,36 @@ class App extends Component {
         super(props)
     }
 
+    handleclick(){
+        alert(1)
+    }
+
     render() {
-        let columns = ['示例1','示例2','示例3']
-        let tdata = ['1','2','3']
+        let columns = ['示例1','示例2','示例3','操作']
+        let tdata = [{
+            id : 1,
+            name : 'hello',
+            email : 'qq@qq.com'
+        },{
+            id : 2,
+            name : 'admin',
+            email : 'admin@qq.com'
+        }]
+        let action = {
+            className : 'col-action clearfix',
+            type : 'html',
+            click : this.handleclick.bind(this),
+            value : ''
+        }
+        let label = '<a class="label '+'add'+'"' + ' data-status="' + '1'+'">' +'添加'+'</a>'
+        action.value = label
+        let cdata = [[1,"hello","qq@qq.com",action],[2,"admin","admin@qq.com",action]]
         return (
             <div className="app">
                 <h1>{packageJson.name}</h1>
                 <h2>{packageJson.description}</h2>
                 <form>
-                    <Table columns={columns} tdata={tdata}></Table>
+                    <Table columns={columns} tdata={cdata}></Table>
                 </form>
             </div>
         )
